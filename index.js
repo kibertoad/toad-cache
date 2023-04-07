0aa0e694be55c49a25d019db464ba99142e915a6
Igor Savin <iselwin@gmail.com>
2023-04-07 18:02:11 +0300

    Add object-based cache

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { fifo } from './src/fifo.js'
 import { lru } from './src/lru.js'
+import { lruObject } from './src/lru-object.js'
 
 export { fifo }
 export { lru }
+export { lruObject }
---
+++ src/lru-object.js
@@ -0,0 +1,159 @@
+class LRU {
+  constructor(max = 0, ttl = 0) {
+    this.first = null
+    this.items = Object.create(null)
+    this.last = null
+    this.max = max
+    this.ttl = ttl
+  }
+
+  get size() {
+    // ToDo compare with explicit counter
+    return Object.keys(this.items).length
+  }
+
+  bumpLru(item) {
+    const last = this.last
+    const next = item.next
+    const prev = item.prev
+
+    if (this.first === item) {
+      this.first = item.next
+    }
+
+    item.next = null
+    item.prev = this.last
+    last.next = item
+
+    if (prev !== null) {
+      prev.next = next
+    }
+
+    if (next !== null) {
+      next.prev = prev
+    }
+
+    this.last = item
+  }
+
+  clear() {
+    this.items = Object.create(null)
+    this.first = null
+    this.last = null
+  }
+
+  delete(key) {
+    if (Object.prototype.hasOwnProperty.call(this.items, key)) {
+      const item = this.items[key]
+
+      delete this.items[key]
+
+      if (item.prev !== null) {
+        item.prev.next = item.next
+      }
+
+      if (item.next !== null) {
+        item.next.prev = item.prev
+      }
+
+      if (this.first === item) {
+        this.first = item.next
+      }
+
+      if (this.last === item) {
+        this.last = item.prev
+      }
+    }
+  }
+
+  evict() {
+    if (this.size > 0) {
+      const item = this.first
+
+      delete this.items[item.key]
+
+      if (this.size === 0) {
+        this.first = null
+        this.last = null
+      } else {
+        this.first = item.next
+        this.first.prev = null
+      }
+    }
+  }
+
+  expiresAt(key) {
+    if (Object.prototype.hasOwnProperty.call(this.items, key)) {
+      return this.items[key].expiry
+    }
+  }
+
+  get(key) {
+    if (Object.prototype.hasOwnProperty.call(this.items, key)) {
+      const item = this.items[key]
+
+      // Item has already expired
+      if (this.ttl > 0 && item.expiry <= Date.now()) {
+        this.delete(key)
+        return
+      }
+
+      // Item is still fresh
+      this.bumpLru(item)
+      return item.value
+    }
+  }
+
+  keys() {
+    return Object.keys(this.items)
+  }
+
+  set(key, value) {
+    // Replace existing item
+    if (Object.prototype.hasOwnProperty.call(this.items, key)) {
+      const item = this.items[key]
+      item.value = value
+
+      item.expiry = this.ttl > 0 ? Date.now() + this.ttl : this.ttl
+
+      if (this.last !== item) {
+        this.bumpLru(item)
+      }
+      return
+    }
+
+    // Add new item
+    if (this.max > 0 && this.size === this.max) {
+      this.evict()
+    }
+
+    const item = {
+      expiry: this.ttl > 0 ? Date.now() + this.ttl : this.ttl,
+      key: key,
+      prev: this.last,
+      next: null,
+      value,
+    }
+    this.items[key] = item
+
+    if (this.size === 1) {
+      this.first = item
+    } else {
+      this.last.next = item
+    }
+
+    this.last = item
+  }
+}
+
+export function lruObject(max = 1000, ttlInMsecs = 0) {
+  if (isNaN(max) || max < 0) {
+    throw new TypeError('Invalid max value')
+  }
+
+  if (isNaN(ttlInMsecs) || ttlInMsecs < 0) {
+    throw new TypeError('Invalid ttl value')
+  }
+
+  return new LRU(max, ttlInMsecs)
+}
