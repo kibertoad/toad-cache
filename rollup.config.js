8012a11b3fa78541d3224ef95a2bc258b76c1ec3
Igor Savin <iselwin@gmail.com>
2023-04-08 17:00:26 +0300

    Backport updated exports from upstream (#3)

--- rollup.config.js
+++ rollup.config.js
@@ -1,53 +1,54 @@
-import terser from '@rollup/plugin-terser'
-import { createRequire } from 'node:module'
-const require = createRequire(import.meta.url)
-const pkg = require('./package.json')
-const year = new Date().getFullYear()
+import terser from "@rollup/plugin-terser";
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
+const pkg = require("./package.json");
+const year = new Date().getFullYear();
 const bannerLong = `/**
  * ${pkg.name}
  *
  * @copyright ${year} ${pkg.author}
  * @license ${pkg.license}
  * @version ${pkg.version}
- */`
+ */`;
 const bannerShort = `/*!
  ${year} ${pkg.author}
  @version ${pkg.version}
-*/`
-const defaultOutBase = { compact: true, banner: bannerLong, name: pkg.name }
-const cjOutBase = { ...defaultOutBase, compact: false, format: 'cjs', exports: 'named' }
-const esmOutBase = { ...defaultOutBase, format: 'esm' }
-const umdOutBase = { ...defaultOutBase, format: 'umd' }
-const minOutBase = { banner: bannerShort, name: pkg.name, plugins: [terser()], sourcemap: true }
+*/`;
+const defaultOutBase = {compact: true, banner: bannerLong, name: pkg.name};
+const cjOutBase = {...defaultOutBase, compact: false, format: "cjs", exports: "named"};
+const esmOutBase = {...defaultOutBase, format: "esm"};
+const umdOutBase = {...defaultOutBase, format: "umd"};
+const minOutBase = {banner: bannerShort, name: pkg.name, plugins: [terser()], sourcemap: true};
 
-const rollupEntries = [{
-    input: `./index.js`,
+
+export default [
+  {
+    input: "./src/lru.js",
     output: [
       {
         ...cjOutBase,
-        file: `dist/${pkg.name}.cjs`,
+        file: `dist/${pkg.name}.cjs`
       },
       {
         ...esmOutBase,
-        file: `dist/${pkg.name}.esm.js`,
+        file: `dist/${pkg.name}.js`
       },
       {
         ...esmOutBase,
         ...minOutBase,
-        file: `dist/${pkg.name}.esm.min.js`,
+        file: `dist/${pkg.name}.min.js`
       },
       {
         ...umdOutBase,
-        file: `dist/${pkg.name}.js`,
-        name: 'toad-cache',
+        file: `dist/${pkg.name}.umd.js`,
+        name: "toad-cache"
       },
       {
         ...umdOutBase,
         ...minOutBase,
-        file: `dist/${pkg.name}.min.js`,
-        name: 'toad-cache',
-      },
-    ],
-  }]
-
-export default rollupEntries
+        file: `dist/${pkg.name}.umd.min.js`,
+        name: "toad-cache"
+      }
+    ]
+  }
+];
